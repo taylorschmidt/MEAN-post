8fa84baa0e3332922356c37d688d279d1b37576a
taylorschmidt <taylor.schmidt@datadoghq.com>
2021-10-29 16:54:35 -0600

    added CORS

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
 const express = require('express')
-
+const bodyParser= require('body-parser')
 const app = express();
 
-app.use('/api/posts',(req, res, next)=>{
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+    res.setHeader('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE, OPTIONS")
+    next();
+})
+app.post('/api/posts',(req, res, next)=>{
+    const post = req.body;
+    console.log(post)
+    res.status(201).json({
+        message: 'post added successfully'
+    })
+});
+
+app.get('/api/posts',(req, res, next)=>{
     const posts = [
         {id: '323532', title: 'Title', content: 'Content'},
         {id: '543453', title: 'Title2', content: 'Content2'}
