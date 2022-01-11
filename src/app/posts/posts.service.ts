b7b376204a8644d7ba73211ba36a3aab75e8a431
taylorschmidt <taylor.schmidt@datadoghq.com>
2022-01-11 12:56:04 -0700

    upload image complete

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -21,7 +21,8 @@ export class PostsService {
                 return {
                     title: post.title,
                     content: post.content,
-                    id: post._id
+                    id: post._id,
+                    imagePath: post.imagePath
                 }
             });
         }))
@@ -44,10 +45,11 @@ export class PostsService {
         postData.append('title', title)
         postData.append('content', content)
         postData.append('image', image, title)
-        const post: Post = {id: null, title: title, content: content}
-        this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
+        // const post: Post = {id: null, title: title, content: content}
+        this.http.post<{message: string; post: Post}>(
+            'http://localhost:3000/api/posts', postData)
         .subscribe((responseData)=>{
-            const post: Post = {id: responseData.postId, title: title, content: content}
+            const post: Post = {id: responseData.post.id, title: title, content: content, imagePath: responseData.post.imagePath}
             this.posts.push(post)
             this.postsUpdated.next([...this.posts])
             this.router.navigate(["/"])
@@ -55,7 +57,7 @@ export class PostsService {
     }
 
     updatePost(id: string, title: string, content: string) {
-        const post: Post = {id: id, title: title, content: content}
+        const post: Post = {id: id, title: title, content: content, imagePath: null}
         this.http.put("http://localhost:3000/api/posts/" + id, post)
             .subscribe(response => {
                 const updatedPosts = [...this.posts];
