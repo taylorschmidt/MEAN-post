3a40e530819306f7359310b7d5e75ba5e5bdee90
taylorschmidt <taylor.schmidt@datadoghq.com>
2022-03-29 12:30:48 -0600

    added backend user group

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const mongoose = require('mongoose')
 const postsRoutes = require('./routes/posts')
+const userRoutes = require('./routes/user')
 const app = express();
 
 mongoose.connect("mongodb+srv://taylor:3KEQKLMzuqboOCvk@cluster0.7anhc.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
@@ -24,6 +25,7 @@ app.use((req, res, next)=>{
 })
 
 app.use("/api/posts", postsRoutes)
+app.use("/api/user", userRoutes)
 
 
 module.exports = app;
